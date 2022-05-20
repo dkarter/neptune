d1bd582cfd425f376e9595714d070b19bd3664c0
Dorian Karter <dkarter@users.noreply.github.com>
2022-05-20 01:01:48 -0500

    feat: show connected nodes

--- lib/neptune_web/controllers/page_controller.ex
+++ lib/neptune_web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule NeptuneWeb.PageController do
   use NeptuneWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    this_node = node()
+    other_nodes = Node.list()
+
+    conn
+    |> render("index.html", %{
+      this_node: this_node,
+      other_nodes: other_nodes
+    })
   end
 end
